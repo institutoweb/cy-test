9611966f034e6b36f1865156e3f47fbbd68c3755
institutoweb <137358271+institutoweb@users.noreply.github.com>
2025-07-03 20:57:36 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://institutoweb.com.ar/test/login.html')
 
-    cy.get('#miusuario').type("UsuarioNuevo")
+    cy.get('#tuusuario').type("UsuarioNuevo")
     cy.get('#tuclave').type("clavesecreta")
     cy.get('#tumail').type("daniel@gmail.com")
     cy.get(':nth-child(8)').click()
